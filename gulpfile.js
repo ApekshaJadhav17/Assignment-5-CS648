925ec4939bf9101cea657ccd3f544dc5b8b58d20
Apeksha Jadhav <apekshajadhav28@gmail.com>
2024-10-04 15:47:40 -0700

    Update gulpfile.js
    gulpfile-update

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,15 @@
 // Task-1 (gulpfile)
-var {src, dest, watch} = require('gulp');
+var { src, dest, watch } = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 sass.compiler = require('node-sass');
 function css() {
-       return src('src/*.scss')
-               .pipe(sass())
-                .pipe(dest('dist/css'))
-                    };
-                    console.log('hi from gulp')
-exports.default = function () 
-{
+    return src('src/*.scss')
+        .pipe(sass())
+        .pipe(dest('dist/css'))
+};
+exports.default = function () {
     watch('src/* .scss', css);
-    console.log('default function executed!')
+};
 
-    };
-                    
 
 
